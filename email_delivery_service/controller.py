bd2abf315164feaa4cea76fbc186f4a2bb4a7c81
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-01-23 21:44:54 +0530

    feat: Send requests to api and update queue status
    * added functions to send emails and update queue status via webhook

---
+++ email_delivery_service/controller.py
@@ -0,0 +1,46 @@
+import frappe
+
+import json
+import requests
+
+def send(self, sender, recipient, msg):
+	""" Send emails to recipients."""
+	data = {
+		"sender": sender,
+		"recipients": recipient,
+		"sk_mail": frappe.get_site_config().get("sk_mail", "fcmailfree100"),
+		"site": frappe.local.site,
+	}
+	files = {"mime": msg}
+
+	resp = requests.post(
+		"https://frappecloud.com/api/method/press.api.email.send_mime_mail",
+		data={"data": json.dumps(data)},
+		files=files,
+	)
+	
+	update_queue_status(self, json.loads(resp.text)["message"])
+
+@frappe.whitelist(allow_guest=True)
+def update_status(**data):
+	"""Update status of queued email via webhook"""
+	docname = frappe.db.get_value(
+		"Email Queue", {"message_id": data["message_id"]}, "name"
+	)
+
+	doc = frappe.get_doc("Email Queue", docname)
+	if data["status"] == "delivered":
+		status = "Sent"
+	elif data["status"] == "failed":
+		status = "Error"
+
+	update_queue_status(doc, status, commit=True)
+
+
+def update_queue_status(queue, status, commit=False):
+	frappe.db.set_value("Email Queue", queue.name, "status", status)
+	if commit:
+		frappe.db.commit()
+	if queue.communication:
+		communication_doc = frappe.get_doc("Communication", queue.communication)
+		communication_doc.set_delivery_status(commit=commit)
