94454c998c76a6b0713a2f3295d76d6f27b9563f
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-01-24 08:58:59 +0530

    feat: Added report for analytics

---
+++ email_delivery_service/email_delivery_service/report/email_delivery_analytics/email_delivery_analytics.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Rutwik Hiwalkar and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Email Delivery Analytics"] = {
+	"filters": [
+
+	]
+};
---
+++ email_delivery_service/email_delivery_service/report/email_delivery_analytics/email_delivery_analytics.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Rutwik Hiwalkar and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
