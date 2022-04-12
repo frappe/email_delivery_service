25f9bf011dcacd6129be9ae6ad2c107a10edc5b2
Rutwik <rutwikhdev@gmail.com>
2022-04-12 12:25:51 +0530

    fix: Added incoming request validation
    * validate incoming requests against the secret/api key

--- email_delivery_service/controller.py
+++ email_delivery_service/controller.py
@@ -34,17 +34,21 @@ def update_status(**data):
 	"""
 	Update status of queued email via webhook
 	"""
-	docname = frappe.db.get_value(
-		"Email Queue", {"message_id": data["message_id"]}, "name"
-	)
+	secret_key = frappe.get_site_config().get("sk_email_delivery_service")
+	if secret_key == data["secret_key"]:
+		docname = frappe.db.get_value(
+			"Email Queue", {"message_id": data["message_id"]}, "name"
+		)
 
-	doc = frappe.get_doc("Email Queue", docname)
-	if data["status"] == "delivered":
-		status = "Sent"
-	elif data["status"] == "failed":
-		status = "Error"
+		doc = frappe.get_doc("Email Queue", docname)
+		if data["status"] == "delivered":
+			status = "Sent"
+		elif data["status"] == "failed":
+			status = "Error"
 
-	update_queue_status(doc, status, commit=True)
+		update_queue_status(doc, status, commit=True)
+
+	return
 
 
 def update_queue_status(queue, status, commit=False):
